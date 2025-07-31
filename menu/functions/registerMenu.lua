ae1e585338baa92ad6f0a2f6a14bd67a0a1e4d51
JLDimitri74 <49373447+JLDimitri74@users.noreply.github.com>
2025-07-31 20:59:22 +0200

    Update registerMenu.lua
    Pour éviter de redémarrer le zUI-v2 à chaque modification, dans un script qui fait appel à getObject()

--- menu/functions/registerMenu.lua
+++ menu/functions/registerMenu.lua
@@ -1,9 +1,47 @@
+
+local MappingForResource = {}
+
 function RegisterMenu(menu)
     local id = menu.id
-    if menu.key and menu.mapping then
-        RegisterCommand(id, function()
-            zUI.SetVisible(id, not zUI.IsVisible(id))
-        end, false)
-        RegisterKeyMapping(id, menu.mapping, "keyboard", menu.key)
+    if menu.key and menu.mapping and menu.Invoke then
+        RegisterMenuSetId(menu.Invoke, menu)
     end
 end
+
+
+function RegisterMenuSetId(ResourceName, menu)
+    if not menu or not ResourceName then
+        return
+    end
+
+    if not MappingForResource[ResourceName] then
+        MappingForResource[ResourceName] = {}
+    end
+
+    if not MappingForResource[ResourceName][menu.key] then
+        local IdResourceKey = ("zUI:MenuIdentifier:%s/%s"):format(ResourceName, menu.key)
+        MappingForResource[ResourceName][menu.key] = menu.id
+        RegisterCommand(IdResourceKey, function()
+            RegisterMenuGetId(ResourceName, menu.key)
+        end, false)
+        RegisterKeyMapping(IdResourceKey, menu.mapping, "keyboard", menu.key)
+    else
+        MappingForResource[ResourceName][menu.key] = menu.id
+    end
+
+end
+
+function RegisterMenuGetId(ResourceName, key)
+    if not MappingForResource[ResourceName] then
+        return nil
+    end
+
+    if not MappingForResource[ResourceName][key] then
+        return nil
+    end
+    if type(zUI) == "table" and zUI.SetVisible and zUI.IsVisible then
+        zUI.SetVisible(MappingForResource[ResourceName][key], not zUI.IsVisible(MappingForResource[ResourceName][key]))
+    end
+end
+
+
